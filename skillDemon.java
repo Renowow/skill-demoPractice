aa5459de18bc55c90962b78a701408b79fa16012
Renowow <103221818+Renowow@users.noreply.github.com>
2022-05-01 17:59:59 -0700

    add file

--- skillDemon.java
+++ skillDemon.java
@@ -1,4 +1,6 @@
 public class skillDemon{
-
+    public static int mutiplication(int a, int b){
+        return a*b;
+    }
 
 }
---
+++ skillDemonTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemonTest {
+    
+    @Test
+    public void mutiplication(){
+        assertEquals(6,skillDemon.mutiplication(2, 4));//should fail initially
+    }
+
+}
